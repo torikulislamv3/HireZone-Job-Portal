f5be59d6e161f03e3940a3b897c32169c8bd3654
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2025-12-13 00:37:54 +0600

    navbar ready for project

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./loogo.svg";
 import search_icon from "./search_icon.svg";
 import company_icon from "./company_icon.svg";
 import microsoft_logo from "./microsoft_logo.svg";
