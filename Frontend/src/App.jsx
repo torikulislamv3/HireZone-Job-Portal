fba7a642befa9fe4a8820b204079ab32db9fc620
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2025-12-20 16:31:43 +0600

    add-job page ready for dashboard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,20 +5,30 @@ import ApplyJob from "./pages/ApplyJob";
 import Applications from "./pages/Applications";
 import RecruiterLogin from "./components/RecruiterLogin";
 import { AppContext } from "./context/AppContext";
+import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/ManageJobs";
+import ViewApplications from "./pages/ViewApplications";
+import 'quill/dist/quill.snow.css';
 
 const App = () => {
-
-
-  const {showRecruiterLogin} = useContext(AppContext);
+  const { showRecruiterLogin } = useContext(AppContext);
 
   return (
     <div>
       {showRecruiterLogin && <RecruiterLogin />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/apply-job/:id" element={<ApplyJob />} />
-        <Route path="/applications" element={<Applications />} />
-      </Routes>
+     <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/apply-job/:id" element={<ApplyJob />} />
+  <Route path="/applications" element={<Applications />} />
+
+  <Route path="/dashboard" element={<Dashboard />}>
+    <Route path="add-job" element={<AddJob />} />
+    <Route path="manage-jobs" element={<ManageJobs />} />
+    <Route path="view-applications" element={<ViewApplications />} />
+  </Route>
+</Routes>
+
     </div>
   );
 };
